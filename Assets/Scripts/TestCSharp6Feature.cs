e47d6ee3339311e64e879f94a004f9bcd5898941
ChengLiang <chengliang@unity3d.com>
2018-03-21 20:14:45 +0800

    add base

---
+++ Assets/Scripts/TestCSharp6Feature.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCSharp6Feature : MonoBehaviour {
+    private Dictionary<int, string> webErrors = new Dictionary<int, string>
+    {
+        [404] = "Page not Found",
+        [302] = "Page moved, but left a forwarding address.",
+        [500] = "The web server can't come out to play today."
+    };
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
